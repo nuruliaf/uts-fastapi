5d908bf3eb794b0d34935c644ee359210174cfaa
nuruliaf <nurulizzaa00@gmail.com>
2021-10-20 17:46:03 +0700

    update auth_handler.py

--- app/auth/auth_handler.py
+++ app/auth/auth_handler.py
@@ -2,11 +2,9 @@ import time
 from typing import Dict
 
 import jwt
-from decouple import config
 
-
-JWT_SECRET = config("secret")
-JWT_ALGORITHM = config("algorithm")
+secret=please_please_update_me_please
+algorithm=HS256
 
 
 def token_response(token: str):
@@ -19,13 +17,13 @@ def signJWT(user_id: str) -> Dict[str, str]:
         "user_id": user_id,
         "expires": time.time() + 600
     }
-    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    token = jwt.encode(payload, secret, algorithm=algorithm)
 
     return token_response(token)
 
 def decodeJWT(token: str) -> dict:
     try:
-        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        decoded_token = jwt.decode(token, secret, algorithms=[algorithm])
         return decoded_token if decoded_token["expires"] >= time.time() else None
     except:
         return {}
